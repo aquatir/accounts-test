5dc8343bbce7f9bb0ecfdeedfc74f8af9aebcaaf
pavel_buhmatov <buhmatov@gmail.com>
2018-12-29 00:04:05 +0300

    Move application initialization to specified method

--- src/main/java/com/revolute/test/example/ApplicationRunner.java
+++ src/main/java/com/revolute/test/example/ApplicationRunner.java
@@ -3,13 +3,19 @@ package com.revolute.test.example;
 import static spark.Spark.*;
 
 public class ApplicationRunner {
+
+
     public static void main(String[] args) {
+        initializeApplication(args);
+
+    }
+
+    public static void initializeApplication(String[] args) {
 
         port(8080);
 
         path("/api", () -> {
             get("/hello", (req, res)->"Hello, world");
         });
-
     }
 }
