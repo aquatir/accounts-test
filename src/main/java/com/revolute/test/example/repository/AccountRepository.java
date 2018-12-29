6c9e5e6c9a6779170a9a6e833b710f736233577d
pavel_buhmatov <buhmatov@gmail.com>
2018-12-30 02:11:18 +0300

    Add DB stub for find for update

--- src/main/java/com/revolute/test/example/repository/AccountRepository.java
+++ src/main/java/com/revolute/test/example/repository/AccountRepository.java
@@ -3,13 +3,27 @@ package com.revolute.test.example.repository;
 import com.revolute.test.example.entity.Account;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
 public class AccountRepository {
 
+    private final String SELECT_FOR_UPDATE_BY_ACCOUNT_NUMBER_PREP_ST = "select * from ACCOUNT for update" +
+            "where number = ?";
+
     public Account findForUpdateByAccountNumber(Connection connection, String accountNumber) {
 
-        return null;
+        try {
+            var prepareSelect = connection.prepareStatement(SELECT_FOR_UPDATE_BY_ACCOUNT_NUMBER_PREP_ST);
+            prepareSelect.setString(1, accountNumber);
+            var resultSet = prepareSelect.executeQuery();
+
+            return null;
+
+        } catch (SQLException sqlException) {
+
+            return null;
+        }
     }
 
     public void saveAll(List<Account> accountFrom) {
